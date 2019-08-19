6ad4dde80ffa7eca83ea0d37088dbde3ee8bd1f8
Tierra Sharae <44881884+TierraSharae@users.noreply.github.com>
2019-08-18 22:14:15 -0500

    HackerRank: Sock Merchant
    https://www.hackerrank.com/challenges/sock-merchant/problem
    
    John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.

---
+++ SockMerchant.java
@@ -0,0 +1,36 @@
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        int total = 0 ; // total count to be returned
+
+        int currCount = 1 ; // count of the current sock number we're on
+
+        int num = 0 ; // the sock "number" itsef
+
+        Arrays.sort(ar) ; 
+
+        for (int i : ar){
+        }
+        
+        num = ar[0] ;
+
+        for (int i = 1 ; i < n ; i++){
+
+            if (ar[i] == num){
+                currCount++;
+            } 
+            if (ar[i] != num || i == n-1) {
+                num = ar[i];
+                if (currCount % 2 == 0) {
+                    total += currCount / 2 ;
+                } else {
+                    total += (currCount-1) / 2 ;
+                }
+                currCount = 1;
+            }
+        }
+             
+        return total;
+    }
